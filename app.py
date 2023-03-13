505b581778c11bfe70f1b0851a7072b4ab5d375b
DNS1196 <107742954+DNS1196@users.noreply.github.com>
2023-03-13 01:40:15 -0400

    Part 3 - project completed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 from flask import Flask,  request, render_template,  redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User, Post
+from models import db, connect_db, User, Post, Tag
 
 app = Flask(__name__)
 
@@ -89,14 +89,17 @@ def delete_user(user_id):
 @app.route('/users/<int:user_id>/posts/new', methods=["GET"])
 def add_post_form(user_id):
     user = User.query.get_or_404(user_id)
-    return render_template('post_form.html', user=user)
+    tags = Tag.query.all()
+    return render_template('post_form.html', user=user, tags=tags)
 
 @app.route('/users/<int:user_id>/posts/new', methods=["POST"])
 def handle_post_form(user_id):
     user = User.query.get_or_404(user_id)
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
     new_post= Post(title=request.form['title'],
                    content= request.form['content'],
-                   user= user)
+                   user= user, tags=tags)
     db.session.add(new_post)
     db.session.commit()
     flash(f"New post '{new_post.title}' added")
@@ -110,13 +113,16 @@ def post_details(post_id):
 @app.route('/posts/<int:post_id>/edit', methods=["GET"])
 def post_details_edit(post_id):
     post= Post.query.get_or_404(post_id)
-    return render_template('post_edit.html', post = post)
+    tags = Tag.query.all()
+    return render_template('post_edit.html', post = post, tags=tags)
 
 @app.route('/posts/<int:post_id>/edit', methods=["POST"])
 def handle_post_detail_edit(post_id):
     post= Post.query.get_or_404(post_id)
     post.title = request.form['title']
     post.content = request.form['content']
+    tag_ids = [int(num) for num in request.form.getlist("tags")]
+    post.tags = Tag.query.filter(Tag.id.in_(tag_ids)).all()
     
     db.session.add(post)
     db.session.commit()
@@ -131,3 +137,59 @@ def delete_post(post_id):
     db.session.commit()
     flash(f"Post '{post.title}' deleted")
     return redirect(f'/users/{post.user_id}')
+
+###################################################################
+
+@app.route('/tags')
+def tag_list():
+    tags = Tag.query.all()
+    return render_template('tag_list.html', tags=tags)
+
+@app.route('/tags/<int:tag_id>')
+def tag_details(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    return render_template('tag_details.html', tag=tag)
+
+@app.route('/tags/new', methods=["GET"])
+def add_tag_form():
+    posts = Post.query.all()
+    return render_template('tag_form.html', posts=posts)
+
+@app.route('/tags/new', methods=["POST"])
+def handle_add_tag_form():
+    post_ids = [int(num) for num in request.form.getlist("posts")]
+    posts = Post.query.filter(Post.id.in_(post_ids)).all()
+    new_tag = Tag(name= request.form['name'], posts=posts)
+    
+    db.session.add(new_tag)
+    db.session.commit()
+    flash(f'Tag {new_tag.name} added')
+    return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>/edit', methods=["GET"])
+def tag_details_edit(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    posts = Post.query.all()
+    return render_template('tag_edit.html', tag=tag, posts=posts)
+
+@app.route('/tags/<int:tag_id>/edit', methods=["POST"])
+def handle_tag_details_edit(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    tag.name= request.form['name']
+    post_ids = [int(num) for num in request.form.getlist("posts")]
+    tag.posts = Post.query.filter(Post.id.in_(post_ids)).all()
+    
+    db.session.add(tag)
+    db.session.commit()
+    flash(f'Tag {tag.name} edited')
+    return redirect('/tags')
+
+@app.route('/tags/<int:tag_id>/delete', methods=["POST"])
+def delete_tag(tag_id):
+    tag = Tag.query.get_or_404(tag_id)
+    
+    db.session.delete(tag)
+    db.session.commit()
+    flash(f'Tag {tag.name} deleted')
+    
+    return redirect('/tags')
--- models.py
+++ models.py
@@ -36,12 +36,30 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, nullable =False,
                            default = datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
-    
+        
     @property
     def friendly_date(self):
         return self.created_at.strftime("%B %d, %Y at %I:%M %p")
 
+class Tag(db.Model):
+    """Tags"""
+    __tablename__ = 'tags'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.Text, nullable=False, unique = True)
+    
+    posts = db.relationship(
+        'Post', secondary='post_tag', backref='tags',lazy='dynamic', cascade="all,delete")
+
+class PostTag(db.Model):
+    """Posts and tags relationship"""
+    __tablename__ = 'post_tag'
+    
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+ 
    
 def connect_db(app):
     db.app = app
     db.init_app(app)
+    
